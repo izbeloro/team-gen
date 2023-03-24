55ef74b8d10be6e0e8fa24d6026dd0fc0698375f
Samantha Orozco <orozcoxs@gmail.com>
2023-03-23 17:45:55 -0700

    fixed css coloring

--- index.js
+++ index.js
@@ -1,108 +1,114 @@
-// node
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./src/page-template.js");
 
-// lib
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
-const Manager = require("./lib/manager");
+// lib 
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-// answers to q's
-const newStaffData = [];
+const newStaffMemberData = [];
 
-// question functinon
-const questions = async() => {
+const questions = async () => {
     const answers = await inquirer
-    .createPromptModule([
-        {
-            type: "input",
-            message: "What is your name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is your ID number?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is your email?",
-            name: "email",
-        },
-        {
-            type: "list",
-            message: "What is your role?",
-            name: ["Engineer", "Intern", "Manager"],
-        },
-    ])
-
-    //manager answers
-    if (answers.role === "Manager") {
-        const managerAns =  await inquirer
-        .createPromptModule([
+        .prompt([
             {
                 type: "input",
-                message: "What is your office number?",
-                name: "officeNumber",
+                message: "What is your name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is your ID number?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is your email?",
+                name: "email",
+            },
+            {
+                type: "list",
+                message: "What is your role?",
+                name: "role",
+                choices: ["Engineer", "Intern", "Manager"],
             },
         ])
+
+
+
+    if (answers.role === "Manager") {
+        const managerAns = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your office number",
+                    name: "officeNumber",
+                },
+            ])
         const newManager = new Manager(
             answers.name,
             answers.id,
             answers.email,
             managerAns.officeNumber
         );
-        newStaffData.push(newManager);
+        newStaffMemberData.push(newManager);
+
+        
     } else if (answers.role === "Engineer") {
         const githubAns = await inquirer
-        .prompt([
-            {
-            type: "input",
-            message: "What is your GitHub user name?",
-            name: "github",
-            }
-        ])
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your GitHub user name?",
+                    name: "github",
+                }
+            ])
         const newEngineer = new Engineer(
             answers.name,
             answers.id,
             answers.email,
             githubAns.github
         );
-        newStaffData.push(newEngineer);
+        newStaffMemberData.push(newEngineer);
+
+        
     } else if (answers.role === "Intern") {
         const internAns = await inquirer
-        .prompt([
-            {
-            type: "input",
-            message: "What university did you attend?",
-            name: "school",
-            },
-        ])
+            .prompt([
+                {
+                    type: "input",
+                    message: "What university did you attend?",
+                    name: "school",
+                },
+            ])
+
         const newIntern = new Intern(
             answers.name,
             answers.id,
             answers.email,
             internAns.school
         );
-        newStaffData.push(newIntern);
+        newStaffMemberData.push(newIntern);
     }
+
 };
 
 async function promptQuestions() {
     await questions()
 
-    const addMemberAns = await inquirer
-    .prompt([
-        {
-        name:'addMember',
-        type: 'list',
-        choices: ['Add a new member', 'Create team'],
-        message: "What would you like to do next?"
-        }
-    ])
 
-    if (addMemberAns.addMember === 'Add a new member')  {
+    const addMemberAns = await inquirer
+        .prompt([
+            {
+                name: 'addMember',
+                type: 'list',
+                choices: ['Add a new member', 'Create team'],
+                message: "What would you like to do next?"
+            }
+        ])
+
+    if (addMemberAns.addMember === 'Add a new member') {
         return promptQuestions()
     }
     return createTeam();
@@ -111,9 +117,10 @@ async function promptQuestions() {
 promptQuestions();
 
 function createTeam() {
-    console.log("newbie", newStaffData)
+    console.log("new guy", newStaffMemberData)
     fs.writeFileSync(
-        "./dist/index.html",
-        generateTeam(newStaffData),
+        "./disc/index.html",
+        generateTeam(newStaffMemberData),
+        "utf-8"
     );
 }
